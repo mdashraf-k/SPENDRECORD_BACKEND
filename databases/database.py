1415a0d986e665fa5b9ccaf4685e0e360bc7ddd1
AsHrAf KaMaL <ashrafk0205@gmail.com>
2026-02-24 19:55:04 +0530

    add supabase postgresql database url

--- databases/database.py
+++ databases/database.py
@@ -4,7 +4,10 @@ from core.config import settings
 
 
 # must remove connect_args when use other databases
-engine = create_engine(settings.database_url)
+engine = create_engine(settings.database_url,
+    pool_pre_ping=True,
+    pool_size=5,
+    max_overflow=10)
 
 # This method for sqlite databases only⬇️
 # engine = create_engine(settings.database_url, connect_args={"check_same_thread": False})
